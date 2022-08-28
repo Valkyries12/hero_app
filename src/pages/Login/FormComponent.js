59a45ed555f8294879585cd05e1f788749da7611
ncaruso <nico.caruso1992@outlook.com>
2022-08-28 17:39:35 -0300

    api login e implementacion Swal

--- src/pages/Login/FormComponent.js
+++ src/pages/Login/FormComponent.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Button, Box } from "@mui/material";
+import Swal from 'sweetalert2';
+import axios from "axios";
 
 const styles = {
   form: {
@@ -9,24 +11,24 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: "10px"
+    padding: "10px",
   },
   input: {
     marginTop: "15px",
     width: "100%",
     padding: "10px 18px",
-    borderRadius: "4px"
+    borderRadius: "4px",
   },
   labelText: {
     color: "#fff",
-    fontSize: "22px"
+    fontSize: "22px",
   },
   errorMessage: {
-    color: "#fff"
+    color: "#fff",
   },
   button: {
-    marginTop: "20px"
-  }
+    marginTop: "20px",
+  },
 };
 
 const FormComponent = () => {
@@ -38,20 +40,39 @@ const FormComponent = () => {
     validationSchema: Yup.object({
       password: Yup.string()
         .max(20, "Debe tener un máximo de 20 caracteres")
-        .min(6, "Debe tener al menos 6 caracteres")
+        .min(4, "Debe tener al menos 4 caracteres")
         .required("Password requerido"),
       email: Yup.string().email("Email inválido").required("Email requerido"),
     }),
     onSubmit: (values) => {
       //aca va la logica de la api para loguear
-      console.log(values)
+      console.log(values);
+      axios
+        .post("http://challenge-react.alkemy.org/", {
+          email: `${values.email}`,
+          password: `${values.password}`,
+        })
+        .then(function (response) {
+          console.log(response);
+          const token = JSON.stringify(response.data.token)
+          localStorage.setItem("token", token);
+        })
+        .catch(function (error) {
+          console.log(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `${error.response.data.error}`,
+          })
+        });
     },
-    
   });
 
   return (
-    <Box component="form"  style={styles.form} onSubmit={formik.handleSubmit}>
-      <label style={styles.labelText} htmlFor="email">Email</label>
+    <Box component="form" style={styles.form} onSubmit={formik.handleSubmit}>
+      <label style={styles.labelText} htmlFor="email">
+        Email
+      </label>
       <input
         id="email"
         name="email"
@@ -61,38 +82,14 @@ const FormComponent = () => {
         value={formik.values.email}
         style={styles.input}
       />
-      {/* <TextField
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-        label="Email"
-        variant="filled"
-        sx={styles.textField}
-      /> */}
-      {/* <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      /> */}
+
       {formik.touched.email && formik.errors.email ? (
         <div style={styles.errorMessage}>{formik.errors.email}</div>
       ) : null}
-      {/* <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      /> */}
-      <label style={styles.labelText} htmlFor="password">Password</label>
+
+      <label style={styles.labelText} htmlFor="password">
+        Password
+      </label>
       <input
         id="password"
         name="password"
@@ -105,8 +102,9 @@ const FormComponent = () => {
       {formik.touched.password && formik.errors.password ? (
         <div style={styles.errorMessage}>{formik.errors.password}</div>
       ) : null}
-      <Button sx={styles.button} type="submit" variant="contained">Enviar</Button>
-
+      <Button sx={styles.button} type="submit" variant="contained">
+        Enviar
+      </Button>
     </Box>
   );
 };
