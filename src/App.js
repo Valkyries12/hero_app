77d1e2091b4164b03ba9cb2a3c636309fa153a01
ncaruso <nico.caruso1992@outlook.com>
2022-08-27 22:34:52 -0300

    estructura de carpetas y dependencias

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Paper } from '@mui/material';
 import './App.css';
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(images/superhero-background.jpeg)`,
+    width: "100vw",
+    height: "100vh"
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.paperContainer}>
+      hello world
     </div>
   );
 }
